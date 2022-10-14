1a3575c2f5cfa83863e0265f567ff17bd1301556
Femerrlo <rm97483@fiap.com.br>
2022-10-13 21:02:44 -0300

    Att aula 13/10'

---
+++ Pasta Fiap/Projeto_Web/intro.js
@@ -0,0 +1,28 @@
+var nomeSistema = "Nome do Sistema";
+let nome = "Alexander Gobbato Paulino Albuquerque"
+let idade = 42.55;
+let isProfessor = true;
+let opção = 'A';
+
+console.log(nomeSistema)
+console.log("Tipo do dado nome: "+ typeof(nome));
+console.log("Tipo de idade: "+ typeof(idade));
+console.log("Tipo do dadod: "+ typeof(opção));
+/*
++ soma
+- sub
+/ divisão
+* multiplicação
+% resto da divisão
+*/
+let num1 = 5;
+let num2 = 10;
+
+console.log("Soma: " + (num1 + num2));
+console.log("Sub: " + (num2 - num1));
+console.log("Mul: " + (num1 * num2));
+console.log("Div: " + (num1 / num2));
+
+
+document.write("<h1>AOBA<h1>")
+document.write("<h2>AOBA<h2>")
