a7a4924d4022707fdfa84892aa599d8e845d95c9
Femerrlo <rm97483@fiap.com.br>
2022-10-27 20:53:11 -0300

    Att ex Python

--- Pasta Fiap/Computational Thinking Usinf Python/ex70.py
+++ Pasta Fiap/Computational Thinking Usinf Python/ex70.py
@@ -1,5 +1,5 @@
 matriz=[]
- 
+retornar=[]
 linhas = int(input('Digite a quantidade de linhas que a matriz terá: '))
 while linhas > 10:
     print("O número de linhas não pode ser maior que 10, por favor digite novamente")
@@ -9,15 +9,21 @@ while colunas > 10:
     print("O número de colunas não pode ser maior que 10, por favor digite novamente")
     colunas = int(input("Digite um novo valor:"))
 
-for i in range(0, linhas, 1):
-    matriz.append([])
+
 for l in range(0, linhas, 1):
     for c in range(0, colunas, 1):    
         num = int(input('Digite um numero: '))
-        matriz[l].append(num)
-print(matriz)
+        matriz[l].append(int(input("Digite um valor: ")))
 
-for l in range(0, linhas, 1):
-    for c in range(0, colunas, 1): 
-      p = int(input("Deseja consultar alguma posição da matriz: "))
-      
+for i in range(0, linhas, 1):
+    print(matriz[i])
+
+while retornar == "S":
+    numero = int(input("Digite um número para ser localizado na matriz: "))
+
+
+    for l in range(0, linhas, 1):
+        for c in range(0, colunas, 1):
+            if (matriz[l][c] == numero):
+                res = res + "[" + 1 + ", " + c + "] "
+                
