be985098d4efa522cd54cea62efc4aabf7c11bd7
Logon Aluno <logonrmlocal@fiap.com.br>
2022-11-01 22:37:58 -0300

    Att01/11

---
+++ Pasta Fiap/Computational Thinking Usinf Python/ex71.py
@@ -0,0 +1,30 @@
+
+busao=[]
+retornar = s
+
+# Cria as linhas na Matriz
+for i in range(0, 10, 1):
+    busao.append([])
+ 
+# Cria para cada linha, cria as colunas na Matriz já gravando o valor digitado
+for l in range(0, 10, 1):
+    for c in range(0, 4, 1):      
+        busao[l].append("-----")
+
+for l in range(0, 10, 1):
+    print(busao[l])
+
+while(retornar=="s"):
+    nome = input("Digite o nome do viajante (até 5 caracteres):  ")
+    fileira = int(input("Digite a fileira que você deseja sentar (1 a 10")) -1
+    cadeira = int(input("Digite a cadeira que você deseja semtar (1 a 4)")) -1
+
+    if (busao[fileira][cadeira] =="-----"):
+        busao [fileira][cadeira] == nome
+        print("reserva feita com sucesso")
+    else:
+        print("Esse assento já esta reservado! ")
+    
+    retornar = input("Deseja realizar mais alguma resrva ? S/N")
+
+print("Mapa das reservas do Ônibus: ")
---
+++ Pasta Fiap/Computational Thinking Usinf Python/ex72.py
@@ -0,0 +1,31 @@
+mm = []
+mt = []
+mn = []
+
+input("Você deseja qual horario para o show ?")
+# Cria as linhas na Matriz
+for i in range(0, 2, 1):
+    mm.append([])
+    
+# Cria para cada linha, cria as colunas na Matriz já gravando o valor digitado
+for l in range(0, 2, 1):
+    for c in range(0, 5, 1):      
+        mm[l].append("-----")
+
+# Cria as linhas na Matriz
+for i in range(0, 2, 1):
+    mt.append([])
+    
+# Cria para cada linha, cria as colunas na Matriz já gravando o valor digitado
+for l in range(0, 2, 1):
+    for c in range(0, 5, 1):      
+        mt[l].append("-----")
+
+# Cria as linhas na Matriz
+for i in range(0, 2, 1):
+    mn.append([])
+    
+# Cria para cada linha, cria as colunas na Matriz já gravando o valor digitado
+for l in range(0, 2, 1):
+    for c in range(0, 5, 1):      
+        mn[l].append("-----")
