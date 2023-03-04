ff9393b31ae651f63aa834358a0a06dc8a55be69
Femerrlo <rm97483@fiap.com.br>
2023-03-03 22:15:03 -0300

    Att python

--- Computational Thinking Usinf Python/exApiCep.py
+++ Computational Thinking Usinf Python/exApiCep.py
@@ -3,7 +3,6 @@ import requests
 cep = input("Digite o CEP desejado: ")
 url = f"https://viacep.com.br/ws/{cep}/json/"
 
-
 response = requests.get(url)
 
 if response.status_code == 200:
@@ -11,5 +10,10 @@ if response.status_code == 200:
     print(f"Logradouro: {dados['logradouro']}")
     print(f"Bairro: {dados['bairro']}")
     print(f"Cidade: {dados['localidade']}")
+    numero = input("Digite o número do endereço: ")
+    complemento = input("Digite o complemento (opcional): ")
+    print(f"Número: {numero}")
+    if complemento:
+        print(f"Complemento: {complemento}")
 else:
     print("CEP não encontrado.")
