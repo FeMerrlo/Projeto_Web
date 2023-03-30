6a16d92c438ebf1172978e98e8b8c2795f15861e
Femerrlo <rm97483@fiap.com.br>
2023-03-30 19:45:50 -0300

    Att Reforco

---
+++ Projeto_Web/reforco_JS2/srcipt.js
@@ -0,0 +1,18 @@
+//caixa de alert
+//alert("Ola Mundo")
+
+//caixa de confirmação
+//let teste=confirm("Deseja sair deste tela ")
+//console.log("O resultado:", teste)
+
+//caixa de prompt
+
+let msg= prompt("Qual é o seu nome")
+console.log("O nome é :", msg)
+document.write("O nome é: ", msg)
+
+
+//IF
+if(10 > 8){
+    document.write("O valor 10 é maior que 7")
+}
