246fcccc0a6b66ee713477225efeace64a6a069c
FeMerrlo <rm97483@fiap.com.br>
2023-04-18 22:37:36 -0300

    Att Python

--- Computational Thinking Usinf Python/CP5.py
+++ Computational Thinking Usinf Python/CP5.py
@@ -11,13 +11,19 @@ for i in range(qtd_produtos):
     produtos.append((nome, qtd, preco_unitario))
 
 
-with open("pedidos.txt", "w") as arquivo:
+try:
+    arquivo = open("pedidos.txt", "a")
     arquivo.write(nome_cliente + "\n")
     for nome, qtd, preco_unitario in produtos:
         arquivo.write(f"{nome},{qtd},{preco_unitario}\n")
+        
+finally:
+    arquivo.close()
+    
 
 opcao = input("Deseja cadastrar um novo pedido? (s/n): ")
-if opcao.lower() == "s":
+if opcao == "s":
+
 
     nome_cliente = input("Digite o seu nome: ")
     qtd_produtos = int(input("Digite a quantidade de produtos que deseja comprar: "))
@@ -32,15 +38,19 @@ if opcao.lower() == "s":
         produtos.append((nome, qtd, preco_unitario))
 
 
-    arquivo.write(nome_cliente + "\n")
-    for nome, qtd, preco_unitario in produtos:
-        arquivo.write(f"{nome},{qtd},{preco_unitario}\n")
+    try:
+        arquivo = open("pedidos.txt", "a")
+        arquivo.write(nome_cliente + "\n")
+        for nome, qtd, preco_unitario in produtos:
+            arquivo.write(f"{nome},{qtd},{preco_unitario}\n")
+    finally:
+        arquivo.close()
 
 total_clientes = {}
-with open("pedidos.txt", "r") as arquivo:
+try:
+    arquivo = open("pedidos.txt", "r")
     nome_cliente = ""
     for linha in arquivo:
-        linha = linha.strip()
         if linha != "":
             if nome_cliente == "":
                 nome_cliente = linha
@@ -53,11 +63,40 @@ with open("pedidos.txt", "r") as arquivo:
                 total_clientes[nome_cliente] += valor_total_produto
         else:
             nome_cliente = ""
+finally:
+    arquivo.close()
 
 
-with open("valor_pedidos.txt", "w") as arquivo:
+try:
+    arquivo = open("valor_pedidos.txt", "w")
     for nome_cliente, valor_total in total_clientes.items():
         arquivo.write(f"{nome_cliente}: R${valor_total:.2f}\n")
+finally:
+    arquivo.close()
+
+total_clientes = {}
+arquivo = open("pedidos.txt", "r")
+nome_cliente = ""
+for linha in arquivo:
+    linha = linha.replace('\n', '')
+    if linha != "":
+        if nome_cliente == "":
+            nome_cliente = linha
+            total_clientes[nome_cliente] = 0
+        else:
+            nome_produto, quantidade, preco_unitario = linha.split(",")
+            quantidade = int(quantidade)
+            preco_unitario = float(preco_unitario)
+            valor_total_produto = quantidade * preco_unitario
+            total_clientes[nome_cliente] += valor_total_produto
+    else:
+        nome_cliente = ""
+arquivo.close()
+
+arquivo = open("total_pedidos.txt", "w")
+for nome_cliente, valor_total in total_clientes.items():
+    arquivo.write(f"{nome_cliente}: R${valor_total:.2f}\n")
+arquivo.close()
 
 
 print("Programa finalizado.")
